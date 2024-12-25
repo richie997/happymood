772df7495228d5d739c0383f82b0d59e8a24a478
richie997@ <obedsonmalonda@gmail.com>
2024-12-25 01:15:37 +0100

    add new component

---
+++ types/index.d.ts
@@ -0,0 +1,49 @@
+declare type SearchParamsProps = {
+  params: { [key: string]: string };
+  SearchParams: { [key: string]: string | string[] | undefined };
+};
+declare type Gender = "Male" | "Female" | "other";
+declare type Status = "pending" | "scheduled" | "cancelled";
+declare interface CreateUserParams {
+  name: string;
+  email: string;
+  phone: string;
+}
+declare interface User extends CreateUserParams {
+  $id: string;
+}
+declare interface RegisterUserParams extends CreateUserParams {
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  occupation: string;
+  emergencyContactName: string;
+  emergencyContactNumber: string;
+  primaryPhysician: string;
+  insuranceProvider: string;
+  insurancePolicyNumber: string;
+  allergies: string | undefined;
+  currentMedication: string | undefined;
+  familyMedicalHistory: string | undefined;
+  pastMedicalHistory: string | undefined;
+  identificationType: string | undefined;
+  identificationNumber: string | undefined;
+  identificationDocument: FormData | undefined;
+  privacyConsent: boolean;
+}
+declare type CreateAppointmentParams = {
+  userId: string;
+  patient: string;
+  primaryPhysician: string;
+  reason: string;
+  schedule: Date;
+  status: Status;
+  note: string | undefined;
+};
+declare type UpdateAppointmentParams = {
+  appointmentId: string;
+  userId: string;
+  timeZone: string;
+  appointment: Appointment;
+  type: string;
+};
